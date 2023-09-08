2abd499a99330f6d63e6becc98ab640d56def59a
yawning5 <yawning055@gmail.com>
2023-09-09 02:55:05 +0900

    Update Demo1ApplicationTests.java

--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+// fd
 @SpringBootTest
 class Demo1ApplicationTests {
 
