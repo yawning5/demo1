6490bbf3264dac2797d0f5030a49fd4e7efaba96
yawn <yawning055@gmail.com>
2023-09-09 03:09:12 +0900

    init

--- src/test/java/com/example/demo/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo/Demo1ApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-// fdfdㅁㄴasd
+// fdfd
 @SpringBootTest
 class Demo1ApplicationTests {
 
