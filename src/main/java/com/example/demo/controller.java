528af8465533c30415513ccac8b3cb58fa82f98e
yawning5 <yawning055@gmail.com>
2023-09-09 02:53:25 +0900

    Update controller.java

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -4,7 +4,7 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//zxc
 @RestController
 @RequestMapping
 public class controller {
